73d6a308fa4f8fd90d1efd5b4429804dd1bcc991
David Pérez <deividpa022@gmail.com>
2023-07-23 22:10:21 -0500

    Tipos, variables y comentarios

---
+++ 1.hello-world/1_hello.py
@@ -0,0 +1,18 @@
+#First Python file
+print("Hello world!")
+print(3.1416)
+print(type(2))
+'''
+Prueba de 
+comentario 
+multilinea
+'''
+
+str_var = "Primera Variable"
+str_var_two = "Segunda variable"
+int_var: int = 4
+
+print(str_var, str_var_two, int_var)
+
+variable: str = "First variable"
+print(variable)
--- 1.hello-world/hello.py
+++
@@ -1,2 +0,0 @@
-#First Python file
-print("Hello world!")
